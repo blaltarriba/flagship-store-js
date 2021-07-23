41a6b672b0600741f4911dd4d5a54beb1d554eb7
Borja Lopez Altarriba <borja.lopez83@gmail.com>
2021-07-23 11:14:40 +0200

    Add test for not found product

--- src/controllers/checkouts.js
+++ src/controllers/checkouts.js
@@ -1,5 +1,15 @@
 function create(request, response) {
   const { product_code } = request.body
+
+  products = ['PEN', 'TSHIRT', 'MUG']
+
+  if (!products.includes(product_code)) {
+    return response.status(404).json(
+      {
+        message: `Product ${product_code} not found`
+      });
+  }
+
   response.json(
     {
       status: 'Success',
--- tests/controllers/checkouts.test.js
+++ tests/controllers/checkouts.test.js
@@ -4,18 +4,33 @@ const app = require('../../src/app');
 describe('Checkouts endpoint', () => {
   describe('POST checkouts', () => {
     it('should create new checkout', async () => {
+      var productCode = 'PEN'
       const body = {
-        product_code: 'PEN',
+        product_code: productCode,
       }
 
       const expectBody = {
         id: '1234',
-        products: ['PEN'],
+        products: [productCode],
       };
 
       const response = await request(app).post('/checkouts').send(body);
 
       expect(response.status).toBe(200);
+      expect(response.body).toHaveProperty('message', expectBody);
+    });
+
+    it('failed when product does not exist', async () => {
+      var productCode = 'FAKE'
+      const body = {
+        product_code: productCode,
+      }
+
+      const expectBody = `Product ${productCode} not found`;
+
+      const response = await request(app).post('/checkouts').send(body);
+
+      expect(response.status).toBe(404);
       expect(response.body).toHaveProperty('message', expectBody);
     });
   });
