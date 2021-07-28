d5960846c9170af1fffb1a279115f628b846f38e
Borja Lopez Altarriba <borja.lopez83@gmail.com>
2021-07-28 22:19:29 +0200

    Add create service tests

---
+++ tests/services/create_checkout.test.js
@@ -0,0 +1,28 @@
+const request = require('supertest-as-promised');
+const app = require('../../src/app');
+const CreateCheckout = require('../../src/services/create_checkout');
+const Checkout = require('../../src/models/checkout');
+const CheckoutRepository = require('../../src/repositories/checkout.repository');
+
+describe('Create checkouts', () => {
+  it('should create new checkout', async () => {
+    var productCode = 'PEN'
+    var expectProducts = [productCode]
+    var uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
+
+    const responseCheckout = CreateCheckout.Do(productCode);
+
+    expect(responseCheckout).toHaveProperty('id');
+    expect(responseCheckout.id).toMatch(uuidRegex);
+    expect(responseCheckout).toHaveProperty('products', expectProducts);
+  });
+
+  it('failed when product does not exist', async () => {
+    var productCode = 'FAKE'
+    const expectBody = `Product ${productCode} not found`;
+
+    const responseCheckout = CreateCheckout.Do(productCode);
+
+    expect(responseCheckout).toBe(null);
+  });
+});
